e9e356e248eb125999c18900df339c4516b24dc5
Erik Arakelayn <erikarakelyan1997@gmail.com>
2016-05-10 12:17:34 +0400

    Names

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var dict = ["Համազասպ Աստվածատրյան", "Գեներալ Ալոշ", "Մարուքե Դրաստամատյան", "Տելեֆոն Սեթո", "Սերգե Կասպառիչ", "Մեռելի Ենոք", "Բոչկա Նիկոլայ", "Բիթլիսցի Արշակ", "Օսեփ Նարիմանով", "Վասիլ", "Ղահվեճի Սեթո", "Հաջի Օնիկ Մանուկոֆ էֆենդին", "Կարո Դարայան", "Պետրոս (Պիեր)", "Նենեթ", "Լեսքյուր", "Զարեհ (Լոխում)", "Պանին", "Ավի", "Գևորգ Աբովյան", "Մեսրոպ Ղազարյան", "Այսել", "Լյովիկ և Ցիգ", "Նաելգատուտ", "Նաերեպրեպ", "Նաերուտսա", "Նախշի", "ընկ․ Վառոդյան", "Նշան Մառանկոզյան"];
+var first = true;
 
 $(window).keypress(function (e) {
 	if (e.keyCode === 13) {
@@ -7,7 +8,13 @@ $(window).keypress(function (e) {
 	}
 })
 function randomWord() {
-	var index = Math.floor((Math.random() * dict.length));
+	if (first === true) {
+		var index = Math.floor((Math.random(5));
+		first = false;
+	}
+	else {
+		var index = Math.floor((Math.random() * dict.length));
+	}
 	$("#guessword").text(dict[index]);
 };
 
